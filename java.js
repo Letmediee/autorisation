d6d34fb9748ac6393f6032232085d1820b611e76
Letmediee <114399580+Letmediee@users.noreply.github.com>
2023-04-11 17:03:57 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    alert("Версия jQuery " + jQuery.fn.jquery);
+});
+$(".dws-form").on("click", ".tab", function() {
+    $(this).addClass('active');
+});
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
